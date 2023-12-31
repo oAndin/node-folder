f96a9262317f86750199df7bc3db5a3ddb3b67e5
oAndin <00andherson@gmail.com>
2023-12-30 23:14:01 -0300

    update files

--- create-simple-server/index.js
+++ create-simple-server/index.js
@@ -1,5 +1,15 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const tempOverviewer = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const dataObj = JSON.parse(data);
+const replaceTemplate = (temp, product) => {
+let output = temp.replace();
+}
 
 const server = http.createServer((req, res) => {
   // console.log(req.url);
@@ -7,10 +17,23 @@ const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the overview!');
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    const cardHtml = dataObj.map(el => replaceTemplate(tempCard, el))
+    res.end(tempOverviewer);
   }
   if (pathName === '/product') {
-    res.end('This is the PRODUCT');
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    res.end(tempProduct);
+  }
+  if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
   }
   else {
     res.writeHead(404, {
